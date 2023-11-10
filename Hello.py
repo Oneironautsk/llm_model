c443d7a5bf39d664f2fb62d07b24e754256cc210
Santhosh Kumar <127672239+Oneironautsk@users.noreply.github.com>
2023-11-10 19:37:10 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("CarTalk AI: Unleashing Automotive Wisdom")
+st.title("LLM-Query Hub : Effortless Data Retrieval And Analysis")
 
-st.header("Question")
+st.write('Ask about features, specs, or anything about cars')
 question = st.text_input('')
 
 if question:
