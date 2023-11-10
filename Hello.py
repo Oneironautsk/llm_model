e2a9aae5824c65903b90c4dc7251345fb0cdd665
Santhosh Kumar <127672239+Oneironautsk@users.noreply.github.com>
2023-11-10 19:41:19 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("LLM-Query Hub : Effortless Data Retrieval And Analysis")
+st.title("LLM-Query Hub : Effortless Data Retrieval And Analysis\n")
 
 st.write(' ')
 
-question = st.text_input('### Ask about features, specs, or anything about cars')
+question = st.text_input('## Ask about features, specs, or anything about cars')
 
 if question:
     chain = get_few_shot_db_chain()
