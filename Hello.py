29d7e9c504298a12ef2f20ea498c445bd7dd8fb7
Santhosh Kumar <127672239+Oneironautsk@users.noreply.github.com>
2023-11-10 19:38:03 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,7 +3,7 @@ from langchain_helper import get_few_shot_db_chain
 
 st.title("LLM-Query Hub : Effortless Data Retrieval And Analysis")
 
-st.write('Ask about features, specs, or anything about cars')
+st.write('### Ask about features, specs, or anything about cars')
 question = st.text_input('')
 
 if question:
