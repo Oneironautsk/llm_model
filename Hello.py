4b4b7706f3d279552ce68da8d8996c5ce7de0a52
Santhosh Kumar <127672239+Oneironautsk@users.noreply.github.com>
2023-11-10 19:25:24 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ from langchain_helper import get_few_shot_db_chain
 st.title("CarTalk AI: Unleashing Automotive Wisdom")
 
 st.header("Question")
-question = st.text_input()
+question = st.text_input('')
 
 if question:
     chain = get_few_shot_db_chain()
